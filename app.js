396db489e4922de20bfbe455f961ccf7686de214
Destiny Boone <destaj.db@gmail.com>
2022-10-06 14:46:35 -0400

    validation, winning & lose case, game over

---
+++ app.js
@@ -0,0 +1,77 @@
+/**
+ * GAME FUNCTION:
+ * Player mus guess a number between a min and mad
+ * Player gets a certain amount of guesses
+ * Notify player of guesses remaining
+ * Notify the player of the correct answer if loose
+ * Let player choose to play again
+*/
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+
+    // Validate
+    if(isNaN(guess)  || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red')
+    }
+
+    // Check if won
+    if(guess === winningNum){ // Game over - won
+        gameOver(true, `${winningNum} is correct! YOU WIN!`)
+    } else {
+        // Wrong number
+        guessesLeft -= 1;
+
+        if(guessesLeft === 0){ // Game over - lost
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`)
+
+        } else { // Game continues - answer wrong
+            // Change border color
+            guessInput.style.borderColor = 'red';
+            
+            // Clear Input
+            guessInput.value= '';
+
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+        }
+    }
+});
+
+// Game Over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set message
+    setMessage(msg, color);
+}
+
+// Set message
+function setMessage(msg, color){
+    message.style.color = color;
+    message.textContent = msg;
+}
+    
